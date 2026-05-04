4a878c9a2ffa27b112fd514ecb644447bff9dedd
malyshkoserhii <malyshkosergiy@gmail.com>
2026-05-04 18:32:55 +0300

    fix: Fixed flake8 linter errors

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "path": file_path,
@@ -14,7 +15,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors": [format_linter_error(error) for error in errors],
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(key, value) for key, value in linter_report.items()
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
     ]
